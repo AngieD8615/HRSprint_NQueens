2917506e9f12c650581a42251ce126bdb1af4837
AngieD8615 <adavidson1@gmail.com>
2020-10-15 20:12:21 -0600

    finished helper functions began working on solver, but still have work to do

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,46 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
+  var curBoard = new Board({n: n}); //fixme
+  let rowIndex = 0;
+  let colIndex = 0;
+
+  helper = (rowIndex, colIndex) => {
+    // base case:
+    // if rowIndex and colIndex = n and there are no conflicts
+    console.log('board', curBoard.rows(), 'rowIndex', rowIndex);
+    if (rowIndex === n && !curBoard.hasAnyRooksConflicts()) {
+      // return curBoard
+      return curBoard;
+    }
+
+    // non base case:
+    // toggle next queen
+    curBoard.togglePiece(rowIndex, colIndex);
+    // numOfRooks++;
+
+    // if no conflict
+    if (!curBoard.hasAnyRooksConflicts()) {
+      // increment rowIndex and colIndex appropiately
+      for (var i = 0; i < n; i++) {
+        if (rowIndex < n) {
+          colIndex = i;
+          // reapply helper
+          console.log(rowIndex, colIndex);
+          return helper(rowIndex + 1, colIndex);
+        }
+      }
+    }
+  };
+
+  helper(rowIndex, colIndex);
+
+
+  for (var i = 0; i < n; i++) {
+    solution.push(curBoard.attributes[i]);
+  }
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +63,20 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  var findSolution = function () {
+    // base case:
+    // if all rows exhasted
+      // increament solution count
+      // stop
+
+
+    // iterate over possible decision
+      // place a piece
+      // recurs into remaining problem
+      // unplace the piece
+  };
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
